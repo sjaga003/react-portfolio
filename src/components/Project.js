e0a793d9067f3ce0f9b412fca1996c728c093d2a
Suhas Jagannath <sjaga003@ucr.edu>
2021-01-05 16:18:12 -0800

    Projects section animations

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,38 +1,148 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import styled from 'styled-components';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const containerFadeIn = {
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+      staggerChildren: 0.2,
+      when: 'beforeChildren',
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 100,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const projectHoverLeft = {
+  show: {
+    y: 0,
+    x: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+  hover: {
+    y: -20,
+    x: 20,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const projectHoverRight = {
+  show: {
+    y: 0,
+    x: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+  hover: {
+    y: -20,
+    x: -20,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const containerHover = {
+  show: {},
+  hover: {},
+};
 
 const Project = ({ index, project }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.25 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('show');
+    }
+  }, [controls, inView]);
+
   return (
-    <ProjectContainer>
+    <ProjectContainer
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={containerFadeIn}
+    >
       {/* If index is even image on left side, if index is odd image on right side */}
       {!(index % 2) ? (
-        <ProjectImage
-          draggable={false}
-          src={project.image}
-          alt={`Project ${index}`}
-        />
+        <>
+          <ImageContainer variants={containerHover} whileHover="hover">
+            <ImageBackground></ImageBackground>
+
+            <ProjectImage
+              draggable={false}
+              src={project.image}
+              alt={`Project ${index}`}
+              variants={projectHoverLeft}
+            />
+          </ImageContainer>
+
+          <ProjectInfo>
+            <ProjectTitle>{project.title}</ProjectTitle>
+            <ProjectDescription>{project.description}</ProjectDescription>
+            <ProjectTechnology>{project.technology}</ProjectTechnology>
+            <ProjectIcons>
+              <IconContainer whileHover={{ color: 'var(--highlight-color)' }}>
+                <ProjectIcon icon={faGithub} />
+              </IconContainer>
+              <IconContainer whileHover={{ color: 'var(--highlight-color)' }}>
+                <ProjectIcon icon={faExternalLinkAlt} />
+              </IconContainer>
+            </ProjectIcons>
+          </ProjectInfo>
+        </>
       ) : (
         ''
       )}
-
-      <ProjectInfo>
-        <ProjectTitle>{project.title}</ProjectTitle>
-        <ProjectDescription>{project.description}</ProjectDescription>
-        <ProjectTechnology>{project.technology}</ProjectTechnology>
-        <ProjectIcons>
-          <ProjectIcon icon={faGithub} />
-          <ProjectIcon icon={faExternalLinkAlt} />
-        </ProjectIcons>
-      </ProjectInfo>
       {index % 2 ? (
-        <ProjectImageRight
-          draggable={false}
-          src={project.image}
-          alt={`Project ${index}`}
-        />
+        <>
+          <ProjectInfo2>
+            <ProjectTitle>{project.title}</ProjectTitle>
+            <ProjectDescription>{project.description}</ProjectDescription>
+            <ProjectTechnology>{project.technology}</ProjectTechnology>
+            <ProjectIcons>
+              <IconContainer whileHover={{ color: 'var(--highlight-color)' }}>
+                <ProjectIcon icon={faGithub} />
+              </IconContainer>
+              <IconContainer whileHover={{ color: 'var(--highlight-color)' }}>
+                <ProjectIcon icon={faExternalLinkAlt} />
+              </IconContainer>
+            </ProjectIcons>
+          </ProjectInfo2>
+          <ImageContainer variants={containerHover} whileHover="hover">
+            <ImageBackground></ImageBackground>
+            <ProjectImage
+              draggable={false}
+              src={project.image}
+              alt={`Project ${index}`}
+              variants={projectHoverRight}
+            />
+          </ImageContainer>
+        </>
       ) : (
         ''
       )}
@@ -40,31 +150,48 @@ const Project = ({ index, project }) => {
   );
 };
 
-const ProjectContainer = styled.div`
+const ProjectContainer = styled(motion.div)`
   display: flex;
   align-items: center;
   padding-bottom: 200px;
 `;
 
-const ProjectImage = styled.img`
+const ProjectImage = styled(motion.img)`
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   border-radius: 14.5408px;
   object-fit: cover;
   width: 400px;
   height: 306px;
-  margin-right: 100px;
+
   user-select: none;
 `;
 
-const ProjectImageRight = styled(ProjectImage)`
-  margin-left: 100px;
-  margin-right: 0px;
+const ImageBackground = styled.div`
+  border: var(--highlight-color) 2px solid;
+  width: 390px;
+  height: 300px;
+  border-radius: 14.5408px;
+  margin-top: 5px;
+  position: absolute;
+`;
+
+const ImageContainer = styled(motion.div)`
+  display: flexbox;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const ProjectInfo = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  margin-left: 80px;
+`;
+
+const ProjectInfo2 = styled(ProjectInfo)`
+  margin-right: 80px;
+  margin-left: 0px;
 `;
 
 const ProjectTitle = styled.h2`
@@ -77,6 +204,7 @@ const ProjectDescription = styled.div`
   font-size: var(--fs-sm);
   padding-bottom: 8px;
   line-height: 22px;
+  width: 450px;
 `;
 
 const ProjectTechnology = styled.div`
@@ -87,6 +215,13 @@ const ProjectTechnology = styled.div`
 `;
 
 const ProjectIcons = styled.div``;
+
+const IconContainer = styled(motion.div)`
+  display: inline-block;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const ProjectIcon = styled(FontAwesomeIcon)`
   margin-right: 12px;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,73 +1,155 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Project from './Project';
 import defaultImage from '../img/default_project.png';
 import styled from 'styled-components';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const containerFadeIn = {
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+      staggerChildren: 0.2,
+      when: 'beforeChildren',
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 100,
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const fillLine = {
+  hidden: {
+    pathLength: 0,
+  },
+  show: {
+    pathLength: 1,
+    transition: {
+      duration: 0.5,
+      when: `afterChildren`,
+    },
+  },
+};
+
+const buttonHover = {
+  show: {
+    background: 'rgba(62,160,230, 0)',
+  },
+  hover: {
+    background: 'rgba(62,160,230, 0.15)',
+    transition: {
+      type: 'tween',
+      ease: 'easeInOut',
+      duration: 0.3,
+    },
+  },
+};
+
+const list = [
+  {
+    title: 'Project 1',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
+       reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
+    technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
+    image: defaultImage,
+    github: 'google.com',
+    live: 'google.com',
+  },
+  {
+    title: 'Project 2',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
+      reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
+    technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
+    image: defaultImage,
+    github: 'google.com',
+    live: 'google.com',
+  },
+  {
+    title: 'Project 3',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
+      reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
+    image: defaultImage,
+    technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
+    github: 'google.com',
+    live: 'google.com',
+  },
+];
 
 const Projects = () => {
-  const list = [
-    {
-      title: 'Project 1',
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
-       reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
-      technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
-      image: defaultImage,
-      github: 'google.com',
-      live: 'google.com',
-    },
-    {
-      title: 'Project 2',
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
-      reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
-      technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
-      image: defaultImage,
-      github: 'google.com',
-      live: 'google.com',
-    },
-    {
-      title: 'Project 3',
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium dignissimos explicabo commodi,
-      reprehenderit quo asperiores dolores ab ducimus laboriosam iste pariatur rem cumque sed adipisci molestiae voluptate ad repellat?`,
-      image: defaultImage,
-      technology: 'Technology1 ⸱ Technology 2 ⸱ Technology 3',
-      github: 'google.com',
-      live: 'google.com',
-    },
-  ];
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('show');
+    }
+  }, [controls, inView]);
 
   return (
     <ProjectsSection>
-      <ProjectsHeading>Some of My Work</ProjectsHeading>
+      <ProjectsHeading
+        ref={ref}
+        variants={containerFadeIn}
+        initial="hidden"
+        animate={controls}
+      >
+        Some of My Work
+        <BreakLine
+          width="520"
+          height="2"
+          viewBox="0 0 520 2"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.path
+            variants={fillLine}
+            d="M0.75 1H519.75"
+            stroke="#D1DDE4"
+            stroke-width="2"
+          />
+        </BreakLine>
+      </ProjectsHeading>
       <ProjectsContainer>
         {list.map((project, index) => {
           return <Project key={index} index={index} project={list[index]} />;
         })}
       </ProjectsContainer>
-      <SeeMoreButton>See More</SeeMoreButton>
+      <SeeMoreButton variants={buttonHover} initial="show" whileHover="hover">
+        See More
+      </SeeMoreButton>
     </ProjectsSection>
   );
 };
 
-const ProjectsSection = styled.section`
+const ProjectsSection = styled(motion.section)`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   padding: 0px 0px 100px 0px;
 `;
 
-const ProjectsContainer = styled.div`
+const ProjectsContainer = styled(motion.div)`
   & > *:last-child {
     padding-bottom: 130px;
   }
 `;
 
-const ProjectsHeading = styled.h1`
+const ProjectsHeading = styled(motion.h1)`
   font-size: var(--fs-heading);
   color: var(--heading-color);
   display: flex;
   flex-direction: row;
   align-items: center;
   padding-bottom: 80px;
-  &::after {
+  /* &::after {
     content: '';
     display: block;
     position: relative;
@@ -75,10 +157,10 @@ const ProjectsHeading = styled.h1`
     width: 500px;
     margin-left: 15px;
     background: var(--heading-color);
-  }
+  } */
 `;
 
-const SeeMoreButton = styled.button`
+const SeeMoreButton = styled(motion.button)`
   margin-right: 48px;
   border: 1px solid #3ea0e6;
   border-radius: 8px;
@@ -89,6 +171,13 @@ const SeeMoreButton = styled.button`
   font-size: var(--fs-sm);
   font-weight: 500;
   align-self: center;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const BreakLine = styled.svg`
+  margin-left: 24px;
 `;
 
 export default Projects;
