0cef09871cb962160740f1cd521bd68aea60e801
Suhas Jagannath <sjaga003@ucr.edu>
2021-04-15 12:58:48 -0700

    Update form success and 404 pages to have consistent styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,33 +1,77 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
+import styled, { ThemeProvider } from 'styled-components';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+import { darkTheme, GlobalStyle } from '../components/GlobalStyles';
+import { useMediaQuery } from 'react-responsive';
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        <Link to="/">Go home</Link>
-      </p>
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <Main>
+        <Content>
+          <Section>
+            <span>404</span>
+          </Section>
+          <Section>
+            Something's wrong. Look's like you went the wrong way. Head back to
+            the <LinkTag to="/">home page</LinkTag>!
+          </Section>
+        </Content>
+        <Footer />
+      </Main>
+    </ThemeProvider>
   );
 };
 
 export default NotFoundPage;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  height: 100%;
+  padding: 6rem 0;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > :first-child {
+    color: var(--heading-color);
+    font-family: Roboto, sans-serif;
+    font-weight: 700;
+    font-size: var(--fs-title);
+    width: 100%;
+    padding-bottom: 2rem;
+  }
+  @media (max-width: 575.98px) {
+    padding: 1rem 1rem;
+    font-size: var(--fs-xs);
+  }
+`;
+
+const Section = styled.div`
+  width: 100%;
+  font-size: var(--fs-md);
+  font-weight: 400;
+  max-width: 40ch;
+  text-align: center;
+`;
+
+const LinkTag = styled(Link)`
+  color: var(--highlight-color);
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
--- src/pages/FormSuccess.js
+++ src/pages/FormSuccess.js
@@ -1,33 +1,74 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, GlobalStyle } from '../components/GlobalStyles';
+import Footer from '../components/Footer';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-
-// markup
 const FormSuccess = () => {
   return (
-    <main style={pageStyles}>
-      <title>Form Submitted</title>
-      <h1 style={headingStyles}>Form Submitted</h1>
-      <p style={paragraphStyles}>
-        <Link to="/">Go home</Link>
-      </p>
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <Main>
+        <Content>
+          <Section>
+            <span>Contact Form Submitted</span>
+          </Section>
+          <Section>
+            Thanks for reaching out. I'll get back to you as soon as possible.
+            Head back to the <LinkTag to="/">home page</LinkTag>!
+          </Section>
+        </Content>
+        <Footer />
+      </Main>
+    </ThemeProvider>
   );
 };
 
 export default FormSuccess;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  height: 100%;
+  padding: 6rem 0;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > :first-child {
+    color: var(--heading-color);
+    font-family: Roboto, sans-serif;
+    font-weight: 700;
+    font-size: var(--fs-title-m);
+    width: 100%;
+    padding-bottom: 2rem;
+  }
+  @media (max-width: 575.98px) {
+    padding: 1rem 1rem;
+    font-size: var(--fs-xs);
+  }
+`;
+
+const Section = styled.div`
+  width: 100%;
+  font-size: var(--fs-md);
+  font-weight: 400;
+  max-width: 40ch;
+  text-align: center;
+`;
+
+const LinkTag = styled(Link)`
+  color: var(--highlight-color);
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
